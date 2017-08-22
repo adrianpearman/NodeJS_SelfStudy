7941fbb6c82af62af2a49a571dd257405c9b01dc
Adrian Pearman <adrianpearman12@gmail.com>
2017-08-22 17:17:37 -0400

    moving files from local storage to github repo

---
+++ NODEJS/app.js
@@ -0,0 +1,6 @@
+// importing other modules to app file
+// the require function takes in a string which is the address of where the other file is in the folder
+// to give access to the function, the require function is set to a variable.
+
+// var greet = require('./greet.js')
+// greet();
